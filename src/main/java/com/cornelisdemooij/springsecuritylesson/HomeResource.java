fbd709887d1e773d1a055918ea772823fb4da5c8
Cornelis de Mooij <cornelis.de.mooij@ing.com>
2020-03-09 13:18:56 +0100

    Added user and admin page, logout links and authorization.

--- src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
@@ -8,6 +8,19 @@ public class HomeResource {
 
     @GetMapping("/")
     public String home() {
-        return ("<h1>Helloooooooo Young Colfield</h1>");
+        return ("<h1>Helloooooooo Young Colfield</h1>" +
+                "<a href='/logout'>Logout</a>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>" +
+                "<a href='/logout'>Logout</a>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>" +
+                "<a href='/logout'>Logout</a>");
     }
 }
