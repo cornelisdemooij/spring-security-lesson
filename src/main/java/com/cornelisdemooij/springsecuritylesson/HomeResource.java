4ac516ff985c1ce80f4f3f20a10b7c000c1f8791
Cornelis de Mooij <cornelis.de.mooij@ing.com>
2020-03-09 01:23:38 +0100

    Removed hardcoded user and password from application.properties, added authentication manager with in memory authentication and no-op password encoder. Users and passwords now defined in code.

--- src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeResource {
+
     @GetMapping("/")
     public String home() {
         return ("<h1>Helloooooooo Young Colfield</h1>");
