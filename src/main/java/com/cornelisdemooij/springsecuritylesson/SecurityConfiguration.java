904096d901660665d1d338c58fee60fce426f841
Cornelis de Mooij <cornelis.de.mooij@ing.com>
2020-04-09 00:39:06 +0200

    Added H2 database, still adding users from code. Also updated the HTML for testing convenience

--- src/main/java/com/cornelisdemooij/springsecuritylesson/SecurityConfiguration.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/SecurityConfiguration.java
@@ -1,26 +1,39 @@
 package com.cornelisdemooij.springsecuritylesson;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Set your configuration on the auth object:
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                    User.withUsername("user")
+                    .password("pass")
+                    .roles("USER")
+            )
+            .withUser(
+                    User.withUsername("admin")
+                    .password("pass")
+                    .roles("ADMIN")
+            );
     }
 
     @Override
