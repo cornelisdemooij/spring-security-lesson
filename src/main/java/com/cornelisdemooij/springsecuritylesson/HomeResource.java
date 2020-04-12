9e58878a5fa2a156291ebea7542525b0391ff382
Cornelis de Mooij <cornelis.de.mooij@ing.com>
2020-04-12 16:07:20 +0200

    Added imports for JWT, implemented authenticate endpoint that returns JWT, using RS512 encryption, updated presentation with instructions for generating private keys

--- src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
@@ -1,10 +1,28 @@
 package com.cornelisdemooij.springsecuritylesson;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cornelisdemooij.springsecuritylesson.models.AuthenticationRequest;
+import com.cornelisdemooij.springsecuritylesson.models.AuthenticationResponse;
+import com.cornelisdemooij.springsecuritylesson.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
 
     private String links = "<a href='/'>Home</a> " +
             "<a href='/user'>User</a> " +
@@ -24,5 +42,25 @@ public class HomeResource {
     @GetMapping("/admin")
     public String admin() {
         return ("<h1>Welcome Admin</h1>" + links);
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(
+            @RequestBody AuthenticationRequest authenticationRequest
+    ) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
--- src/main/java/com/cornelisdemooij/springsecuritylesson/SecurityConfiguration.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package com.cornelisdemooij.springsecuritylesson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -26,11 +28,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/authenticate").permitAll()
                 .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
                 .and().formLogin();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
