dff519201fcade9f23ca2b0654840ee312fdbc1c
Cornelis de Mooij <codemo@gmail.com>
2020-05-11 23:25:49 +0200

    Removed all the code that wasn't necessary for OAuth authentication to make the simplest possible example. Also updated the Slides

--- src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
+++ src/main/java/com/cornelisdemooij/springsecuritylesson/HomeResource.java
@@ -1,68 +1,15 @@
 package com.cornelisdemooij.springsecuritylesson;
 
-import com.cornelisdemooij.springsecuritylesson.models.AuthenticationRequest;
-import com.cornelisdemooij.springsecuritylesson.models.AuthenticationResponse;
-import com.cornelisdemooij.springsecuritylesson.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeResource {
-
-    /*@Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Qualifier("userDetailsServiceBean")
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private JwtUtil jwtUtil;*/
-
-    private String links = "<a href='/'>Home</a> " +
-            "<a href='/user'>User</a> " +
-            "<a href='/admin'>Admin</a> " +
+    private String links = "<br><a href='/'>Home</a> " +
             "<a href='/logout'>Logout</a>";
 
     @GetMapping("/")
-    public String home() {
-        return ("<h1>Helloooooooo Young Colfield</h1>" + links);
-    }
-
-    @GetMapping("/user")
     public String user() {
-        return ("<h1>Welcome User</h1>" + links);
+        return ("<h1>Welkom gebruiker!</h1>" +
+                "Deze pagina hoort alleen zichtbaar te zijn voor ingelogde gebruikers." + links);
     }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return ("<h1>Welcome Admin</h1>" + links);
-    }
-
-    /*@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(
-            @RequestBody AuthenticationRequest authenticationRequest
-    ) throws Exception {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            authenticationRequest.getUsername(),
-                            authenticationRequest.getPassword()
-                    )
-            );
-        } catch (BadCredentialsException e) {
-            throw new Exception("Incorrect username or password", e);
-        }
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
-    }*/
 }
--- src/main/resources/data_bcrypt.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (username, password, enabled)
-    values ('user', '$2a$10$lugeq4PS.g4wllkNgdBGgOjWNF9v5PjbH6jIjccnuttRBGiIDVehG', true);
-INSERT INTO users (username, password, enabled)
-    values ('admin', '$2a$10$lugeq4PS.g4wllkNgdBGgOjWNF9v5PjbH6jIjccnuttRBGiIDVehG', true);
-
-INSERT INTO authorities (username, authority)
-    values ('user', 'ROLE_USER');
-INSERT INTO authorities (username, authority)
-    values ('admin', 'ROLE_ADMIN');
